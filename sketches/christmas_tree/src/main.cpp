eef5c1d00004a48f4340daf105fd3423c5e00b13
jakeloggins <jake.loggins@gmail.com>
2016-11-28 18:09:05 -0500

    fun fade

--- sketches/christmas_tree/src/main.cpp
+++ sketches/christmas_tree/src/main.cpp
@@ -89,11 +89,71 @@ static uint32_t MQTTlimit = 300;
   //NeoPixelBus<NeoRgbwFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
 
 
-  // -- animator
-  NeoPixelAnimator RandomColorAnim(PixelCount, NEO_CENTISECONDS);
+  // -- fun fade
+    uint16_t effectState = 0;  // general purpose variable used to store effect state
+    const uint8_t AnimationChannels = 1; // we only need one as all the pixels are animated at once
+    NeoPixelAnimator FunFadeAnim(AnimationChannels);
 
+    struct FunFadeState
+    {
+        RgbColor StartingColor;
+        RgbColor EndingColor;
+    };
+    
+    // one entry per pixel to match the animation timing manager
+    FunFadeState FunFadeAnimationState[AnimationChannels];
 
-  // fun loop
+    // simple blend function
+    void BlendAnimUpdate(const AnimationParam& param)
+    {
+        // this gets called for each animation on every time step
+        // progress will start at 0.0 and end at 1.0
+        // we use the blend function on the RgbColor to mix
+        // color based on the progress given to us in the animation
+        RgbColor updatedColor = RgbColor::LinearBlend(
+            FunFadeAnimationState[param.index].StartingColor,
+            FunFadeAnimationState[param.index].EndingColor,
+            param.progress);
+
+        // apply the color to the strip
+        for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
+        {
+            strip.SetPixelColor(pixel, updatedColor);
+        }
+    }
+
+    void FadeInFadeOutRinseRepeat(float luminance)
+    {
+        if (effectState == 0)
+        {
+            // Fade upto a random color
+            // we use HslColor object as it allows us to easily pick a hue
+            // with the same saturation and luminance so the colors picked
+            // will have similiar overall brightness
+            RgbColor target = HslColor(random(360) / 360.0f, 1.0f, luminance);
+            uint16_t time = random(800, 2000);
+
+            FunFadeAnimationState[0].StartingColor = strip.GetPixelColor(0);
+            FunFadeAnimationState[0].EndingColor = target;
+
+            FunFadeAnim.StartAnimation(0, time, BlendAnimUpdate);
+        }
+        else if (effectState == 1)
+        {
+            // fade to black
+            uint16_t time = random(600, 700);
+
+            FunFadeAnimationState[0].StartingColor = strip.GetPixelColor(0);
+            FunFadeAnimationState[0].EndingColor = RgbColor(0);
+
+            FunFadeAnim.StartAnimation(0, time, BlendAnimUpdate);
+        }
+
+        // toggle to the next effect state
+        effectState = (effectState + 1) % 2;
+    }
+
+  // -- fun loop
     const uint16_t AnimCount = PixelCount / 5 * 2 + 1; // we only need enough animations for the tail and one extra
     const uint16_t PixelFadeDuration = 300; // third of a second
     // one second divide by the number of pixels = loop once a second
@@ -163,152 +223,163 @@ static uint32_t MQTTlimit = 300;
         }
     }
 
+  // -- random color
+    NeoPixelAnimator RandomColorAnim(PixelCount, NEO_CENTISECONDS);
+    void SetupRandomColor()
+    {
+        // setup some animations
+        for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
+        {
+            const uint8_t peak = 128;
+
+            // pick a random duration of the animation for this pixel
+            // since values are centiseconds, the range is 1 - 4 seconds
+            uint16_t time = random(100, 400);
+
+            // each animation starts with the color that was present
+            RgbColor originalColor = strip.GetPixelColor(pixel);
+            // and ends with a random color
+            RgbColor targetColor = RgbColor(random(peak), random(peak), random(peak));
+            // with the random ease function
+            AnimEaseFunction easing;
+
+            switch (random(3))
+            {
+            case 0:
+                easing = NeoEase::CubicIn;
+                break;
+            case 1:
+                easing = NeoEase::CubicOut;
+                break;
+            case 2:
+                easing = NeoEase::QuadraticInOut;
+                break;
+            }
 
 
-  RgbColor red(colorSaturation, 0, 0);
-  RgbColor green(0, colorSaturation, 0);
-  RgbColor blue(0, 0, colorSaturation);
-  RgbColor white(colorSaturation);
-  RgbColor black(0);
+          // we must supply a function that will define the animation, in this example
+          // we are using "lambda expression" to define the function inline, which gives
+          // us an easy way to "capture" the originalColor and targetColor for the call back.
+          //
+          // this function will get called back when ever the animation needs to change
+          // the state of the pixel, it will provide a animation progress value
+          // from 0.0 (start of animation) to 1.0 (end of animation)
+          //
+          // we use this progress value to define how we want to animate in this case
+          // we call RgbColor::LinearBlend which will return a color blended between
+          // the values given, by the amount passed, hich is also a float value from 0.0-1.0.
+          // then we set the color.
+          //
+          // There is no need for the MyAnimationState struct as the compiler takes care
+          // of those details for us
+          
+          
+          AnimUpdateCallback animUpdate = [=](const AnimationParam& param)
+          {
+              // progress will start at 0.0 and end at 1.0
+              // we convert to the curve we want
+              float progress = easing(param.progress);
 
-  HslColor hslRed(red);
-  HslColor hslGreen(green);
-  HslColor hslBlue(blue);
-  HslColor hslWhite(white);
-  HslColor hslBlack(black);
+              // use the curve value to apply to the animation
+              RgbColor updatedColor = RgbColor::LinearBlend(originalColor, targetColor, progress);
+              strip.SetPixelColor(pixel, updatedColor);
+          };
+
+
+          // now use the animation properties we just calculated and start the animation
+          // which will continue to run and call the update function until it completes
+          RandomColorAnim.StartAnimation(pixel, 10000, animUpdate);
+
+        }
+    }
+
+
+
+
+  // solid color stuff
+
+    RgbColor red(colorSaturation, 0, 0);
+    RgbColor green(0, colorSaturation, 0);
+    RgbColor blue(0, 0, colorSaturation);
+    RgbColor white(colorSaturation);
+    RgbColor black(0);
+
+    HslColor hslRed(red);
+    HslColor hslGreen(green);
+    HslColor hslBlue(blue);
+    HslColor hslWhite(white);
+    HslColor hslBlack(black);
 
   const uint16_t global_wait = 50;
 
-  // Input a value 0 to 255 to get a color value.
-  // The colours are a transition r - g - b - back to r.
-  RgbColor Wheel(byte WheelPos) {
-      uint8_t redWheel = 0;
-      uint8_t greenWheel = 0;
-      uint8_t blueWheel = 0;
-      yield();
-      
-    WheelPos = 255 - WheelPos;
-    if(WheelPos < 85) {
-        redWheel = (255 - WheelPos * 3);
-        greenWheel = 0;
-        blueWheel = WheelPos * 3;
-        return RgbColor(redWheel, greenWheel, blueWheel);
-        //return WheelColor;
-    } else if(WheelPos < 170) {
-        WheelPos -= 85;
-        redWheel = 0;
-        greenWheel = WheelPos * 3;
-        blueWheel = (255 - WheelPos * 3);
-        return RgbColor(redWheel, greenWheel, blueWheel);
-        //return WheelColor;
-    } else {
-        WheelPos -= 170;
-        redWheel = WheelPos * 3;
-        greenWheel = (255 - WheelPos * 3);
-        blueWheel = 0;
-        return RgbColor(redWheel, greenWheel, blueWheel);
-        //return WheelColor;
-    }
-  }
+  // -- assorted other functions
 
-  void rainbow(uint8_t wait) {
-    uint16_t i, j;
-
-    for(j=0; j<256; j++) {
-      for(i=0; i<PixelCount; i++) {
-        strip.SetPixelColor(i, Wheel((i+j) & 255));
-      }
-      strip.Show();
-      delay(wait);
-    }
-  }
-
-  void SetRandomSeed()
-  {
-      uint32_t seed;
-
-      // random works best with a seed that can use 31 bits
-      // analogRead on a unconnected pin tends toward less than four bits
-      seed = analogRead(0);
-      delay(1);
-
-      for (int shifts = 3; shifts < 31; shifts += 3)
-      {
-          seed ^= analogRead(0) << shifts;
-          delay(1);
-      }
-
-      // Serial.println(seed);
-      randomSeed(seed);
-  }
-
-  void SetupRandomColor()
-  {
-      // setup some animations
-      for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
-      {
-          const uint8_t peak = 128;
-
-          // pick a random duration of the animation for this pixel
-          // since values are centiseconds, the range is 1 - 4 seconds
-          uint16_t time = random(100, 400);
-
-          // each animation starts with the color that was present
-          RgbColor originalColor = strip.GetPixelColor(pixel);
-          // and ends with a random color
-          RgbColor targetColor = RgbColor(random(peak), random(peak), random(peak));
-          // with the random ease function
-          AnimEaseFunction easing;
-
-          switch (random(3))
-          {
-          case 0:
-              easing = NeoEase::CubicIn;
-              break;
-          case 1:
-              easing = NeoEase::CubicOut;
-              break;
-          case 2:
-              easing = NeoEase::QuadraticInOut;
-              break;
-          }
-
-
-        // we must supply a function that will define the animation, in this example
-        // we are using "lambda expression" to define the function inline, which gives
-        // us an easy way to "capture" the originalColor and targetColor for the call back.
-        //
-        // this function will get called back when ever the animation needs to change
-        // the state of the pixel, it will provide a animation progress value
-        // from 0.0 (start of animation) to 1.0 (end of animation)
-        //
-        // we use this progress value to define how we want to animate in this case
-        // we call RgbColor::LinearBlend which will return a color blended between
-        // the values given, by the amount passed, hich is also a float value from 0.0-1.0.
-        // then we set the color.
-        //
-        // There is no need for the MyAnimationState struct as the compiler takes care
-        // of those details for us
+    // Input a value 0 to 255 to get a color value.
+    // The colours are a transition r - g - b - back to r.
+    RgbColor Wheel(byte WheelPos) {
+        uint8_t redWheel = 0;
+        uint8_t greenWheel = 0;
+        uint8_t blueWheel = 0;
+        yield();
         
-        
-        AnimUpdateCallback animUpdate = [=](const AnimationParam& param)
+      WheelPos = 255 - WheelPos;
+      if(WheelPos < 85) {
+          redWheel = (255 - WheelPos * 3);
+          greenWheel = 0;
+          blueWheel = WheelPos * 3;
+          return RgbColor(redWheel, greenWheel, blueWheel);
+          //return WheelColor;
+      } else if(WheelPos < 170) {
+          WheelPos -= 85;
+          redWheel = 0;
+          greenWheel = WheelPos * 3;
+          blueWheel = (255 - WheelPos * 3);
+          return RgbColor(redWheel, greenWheel, blueWheel);
+          //return WheelColor;
+      } else {
+          WheelPos -= 170;
+          redWheel = WheelPos * 3;
+          greenWheel = (255 - WheelPos * 3);
+          blueWheel = 0;
+          return RgbColor(redWheel, greenWheel, blueWheel);
+          //return WheelColor;
+      }
+    }
+
+    void rainbow(uint8_t wait) {
+      uint16_t i, j;
+
+      for(j=0; j<256; j++) {
+        for(i=0; i<PixelCount; i++) {
+          strip.SetPixelColor(i, Wheel((i+j) & 255));
+        }
+        strip.Show();
+        delay(wait);
+      }
+    }
+
+    void SetRandomSeed()
+    {
+        uint32_t seed;
+
+        // random works best with a seed that can use 31 bits
+        // analogRead on a unconnected pin tends toward less than four bits
+        seed = analogRead(0);
+        delay(1);
+
+        for (int shifts = 3; shifts < 31; shifts += 3)
         {
-            // progress will start at 0.0 and end at 1.0
-            // we convert to the curve we want
-            float progress = easing(param.progress);
+            seed ^= analogRead(0) << shifts;
+            delay(1);
+        }
 
-            // use the curve value to apply to the animation
-            RgbColor updatedColor = RgbColor::LinearBlend(originalColor, targetColor, progress);
-            strip.SetPixelColor(pixel, updatedColor);
-        };
+        // Serial.println(seed);
+        randomSeed(seed);
+    }
 
 
-        // now use the animation properties we just calculated and start the animation
-        // which will continue to run and call the update function until it completes
-        RandomColorAnim.StartAnimation(pixel, 10000, animUpdate);
 
-      }
-  }
+
 
 
 
@@ -476,7 +547,8 @@ static uint32_t MQTTlimit = 300;
           else if (lookup_val == "animationMenu") {
             //neoPixelChange = true;
             //SetupRandomColor();
-            FunLoopAnim.StartAnimation(0, NextPixelMoveDuration, FunLoopAnimUpdate);
+            //FunLoopAnim.StartAnimation(0, NextPixelMoveDuration, FunLoopAnimUpdate);
+            FadeInFadeOutRinseRepeat(0.2f); // 0.0 = black, 0.25 is normal, 0.5 is bright
           }
           /*
           else if (lookup_val == "THIRD STATIC ENDPOINT ID") {
@@ -620,6 +692,12 @@ void loop() {
   }
   */
 
+
+  if (FunFadeAnim.IsAnimating()) {
+      // the normal loop just needs these two to run the active animations
+      FunFadeAnim.UpdateAnimations();
+      strip.Show();
+  }
 
   if (FunLoopAnim.IsAnimating()) {
       // the normal loop just needs these two to run the active animations
