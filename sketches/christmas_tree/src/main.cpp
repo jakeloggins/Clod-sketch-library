51619b5e94bbf96946f9f6e62e5badc5913c1fd8
jakeloggins <jake.loggins@gmail.com>
2016-11-29 11:43:21 -0500

    random color 5 seconds

--- sketches/christmas_tree/src/main.cpp
+++ sketches/christmas_tree/src/main.cpp
@@ -354,7 +354,7 @@ static uint32_t MQTTlimit = 300;
 
           // now use the animation properties we just calculated and start the animation
           // which will continue to run and call the update function until it completes
-          RandomColorAnim.StartAnimation(pixel, 1000, animUpdate);
+          RandomColorAnim.StartAnimation(pixel, 500, animUpdate);
 
         }
     }
