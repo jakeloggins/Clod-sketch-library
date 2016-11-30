6c948c53f4cbff5680f481f1cb576ed2e6c23394
jakeloggins <jake.loggins@gmail.com>
2016-11-29 20:03:40 -0500

    rainbow float fix

--- sketches/christmas_tree/src/main.cpp
+++ sketches/christmas_tree/src/main.cpp
@@ -387,7 +387,9 @@ static uint32_t MQTTlimit = 300;
           if (pixelOrigHueValue > 255) {
             pixelOrigHueValue = pixelOrigHueValue - 255;
           }
-          HslColor pixelOriginalHue (pixelOrigHueValue/360.0f, 1.0f, 0.5f);
+          float pixelOriginHueValueFloat = (float) pixelOriginalHueValue;
+
+          HslColor pixelOriginalHue (pixelOrigHueValueFloat/360.0f, 1.0f, 0.5f);
 
           // and ends with the original plus 254 rolled over
           uint16_t pixelFinalHueValue = pixelOrigHueValue + 254;
@@ -395,7 +397,9 @@ static uint32_t MQTTlimit = 300;
           if (pixelFinalHueValue > 255) {
             pixelFinalHueValue = pixelFinalHueValue - 255;
           }
-          HslColor pixelFinalHue (pixelFinalHueValue/360.0f, 1.0f, 0.5f);
+          float pixelOriginHueValueFloat = (float) pixelOriginalHueValue;
+
+          HslColor pixelFinalHue (pixelFinalHueValueFloat/360.0f, 1.0f, 0.5f);
 
 
           // with the random ease function
