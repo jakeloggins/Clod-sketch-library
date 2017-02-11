75254281c2b50fcee3726b350036b07744b6afa6
jakeloggins <jake.loggins@gmail.com>
2017-02-11 13:40:33 -0500

    three servos

--- sketches/three_servos/src/main.cpp
+++ sketches/three_servos/src/main.cpp
@@ -4,19 +4,96 @@
 #include <ArduinoOTA.h>
 #include <TimeLib.h>
 
+
 //for LED status
 #include <Ticker.h>
 Ticker ticker;
 
+
+uint32_t t = 0;
+
+
 // -- Servo setup
 
-  #include <Servo.h>
-  Servo firstServo;
+  #include <Wire.h>
+  #include <Adafruit_PWMServoDriver.h>
+
+  // called this way, it uses the default address 0x40
+  Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+  // you can also call it with a different address you want
+  //Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);  
+
+  #define SERVOMIN  100 // this is the 'minimum' pulse length count (out of 4096)
+  #define SERVOMAX  450 // this is the 'maximum' pulse length count (out of 4096)
+
+
+  uint16_t pulselen;
+  int selectedServo = 0;
+  bool servoFlag = false;
+
   int selectedPos;
-  int actualPos;
-  int pos;
+  int selectedOpen = 175;
+  int selectedClose = 5;
   static uint32_t lastMove = 0;
-  static uint32_t moveLimit = 100;
+  static uint32_t moveLimit = 250;
+
+  int selectedFanSpeed = 90;
+
+
+
+  void servoRun() {
+
+    if (millis() - lastMove > moveLimit) {
+      lastMove = millis();
+     
+      yield();
+      // convert to PWM using map function
+      pulselen = map(selectedPos, 0, 180, SERVOMIN, SERVOMAX);
+      Serial.printf("selected servo .. %d \n", selectedServo);
+      Serial.printf("selected pos .. %d \n", selectedPos);
+      Serial.printf("pulse .. %d \n", pulselen);
+      // set PWM
+      pwm.setPWM(selectedServo, 0, pulselen);
+
+    }
+    
+  }
+
+
+// -- fan stuff
+  bool fanToggle = false;
+
+
+
+// -- temp setup
+  int counter = 5;
+  #include <OneWire.h>
+  #include <DallasTemperature.h>
+  #define ONE_WIRE_BUS 13
+
+  // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+  OneWire oneWire(ONE_WIRE_BUS);
+
+  // Pass our oneWire reference to Dallas Temperature. 
+  DallasTemperature DS18B20(&oneWire);
+  char temperatureCString[6];
+  char temperatureFString[6];
+
+  float tempC;
+  float tempF;
+
+  void getTemperature() {
+
+    do {
+      DS18B20.requestTemperatures(); 
+      tempC = DS18B20.getTempCByIndex(0);
+      dtostrf(tempC, 2, 2, temperatureCString);
+      tempF = DS18B20.getTempFByIndex(0);
+      dtostrf(tempF, 3, 2, temperatureFString);
+      delay(100);
+    } while (tempC == 85.0 || tempC == (-127.0));
+  }
+
 
 
 // -- global info --
@@ -30,8 +107,8 @@ Ticker ticker;
   #include <wifilogin.h>
     // located in platformio library folder (usually [home]/.platformio/lib)
     // stores const char ssid and const char password
-
-
+  
+  boolean controlFlag = false;
   String chip_id = String(ESP.getChipId());
   String confirmPath = "";
   String confirmPayload = "";
@@ -40,7 +117,7 @@ Ticker ticker;
   String error_path = "";
 
   static uint32_t MQTTtick = 0;
-  static uint32_t MQTTlimit = 300;
+  static uint32_t MQTTlimit = 500;
 
 // -- MQTT server setup
   #include <PubSubClient.h>
@@ -52,9 +129,10 @@ Ticker ticker;
   PubSubClient client(espClient, server, 1883);
   boolean sendConfirm = false;
 
-  // wait time for displaying NTP
+// wait time for displaying NTP
   static uint32_t tick = 0;
   static uint32_t tickLimit = 30000;
+
 
 // -- TICK FUNTION
   void tick_fnc()
@@ -100,13 +178,17 @@ Ticker ticker;
 
 
 
+  // -- MQTTtick and MQTTlimit are here so that a massive flood of MQTT messages don't overwhelm the chip and cause it to restart.
+  // -- But it doesn't really work that well. Increasing the limit time only helps slightly. Ideally, something external would introduce 
+  // -- a 200ms - 300ms delay between consecutive messages.
   String topic;
   String payload;
   void callback(const MQTT::Publish& pub) {
     yield();
+    client.loop();
+
     if (millis() - MQTTtick > MQTTlimit) {
       MQTTtick = millis();
-
 
       int commandLoc;
       String command = "";
@@ -175,7 +257,7 @@ Ticker ticker;
 
         // get topic variables
         maxitems = i;
-
+        controlFlag = false;
         for (i=1; i<maxitems; i++) {
           String chunk = getValue(topic, '/', i);
           if (chunk == "control") {
@@ -183,9 +265,12 @@ Ticker ticker;
             command = chunk;
             deviceName = getValue(topic, '/', i + 1);
             endPoint = getValue(topic, '/', i + 2);
+            controlFlag = true;
             break;
           }
         }
+
+        if (controlFlag) {
 
         //Serial.println("device and endpoint incoming...");
         //Serial.println(deviceName);
@@ -216,24 +301,152 @@ Ticker ticker;
               Serial.begin(115200);
             }
 
-            //neoPixelChange = true;
           }
           
-          else if (lookup_val == "servo") {
+          else if (lookup_val == "servoOpen") {
             String findValue = getValue(payload, ':', 1);
             findValue.remove(findValue.length() - 1);
 
-            selectedPos = findValue.toInt();
+            Serial.println("servoOpen");
+
+
+            selectedOpen = findValue.toInt();
 
           }
-          /*
+          else if (lookup_val == "servoClose") {
+            String findValue = getValue(payload, ':', 1);
+            findValue.remove(findValue.length() - 1);
+
+            Serial.println("servoClose");
+            
+            
+            selectedClose = findValue.toInt();
+
+          }
+/*          else if (lookup_val == "servoToggle") {
+            String findValue = getValue(payload, ':', 1);
+            findValue.remove(findValue.length() - 1);
+
+            Serial.println("servoToggle");
+
+            selectedServo = 0;
+
+            if (findValue == "true") {
+              selectedPos = selectedOpen;
+            }
+            else if (findValue == "false") {
+              selectedPos = selectedClose;
+            }
+
+            servoFlag = true;
+          }*/
+
+           reserved for future use
+         else if (lookup_val == "servoToggleZero") {
+            String findValue = getValue(payload, ':', 1);
+            findValue.remove(findValue.length() - 1);
+
+            selectedServo = 0;
+            servoFlag = true;
+
+            if (findValue == "true") {
+              selectedPos = selectedOpen;
+            }
+            else if (findValue == "false") {
+              selectedPos = selectedClose;
+            }
+          }
+
+         else if (lookup_val == "servoToggleOne") {
+            String findValue = getValue(payload, ':', 1);
+            findValue.remove(findValue.length() - 1);
+
+            selectedServo = 1;
+            servoFlag = true;
+
+            if (findValue == "true") {
+              selectedPos = selectedOpen;
+            }
+            else if (findValue == "false") {
+              selectedPos = selectedClose;
+            }
+          }
+
+         else if (lookup_val == "servoToggleTwo") {
+            String findValue = getValue(payload, ':', 1);
+            findValue.remove(findValue.length() - 1);
+
+            selectedServo = 2;
+            servoFlag = true;
+
+            if (findValue == "true") {
+              selectedPos = selectedOpen;
+            }
+            else if (findValue == "false") {
+              selectedPos = selectedClose;
+            }
+          }
+
+          
           else if (lookup_val == "fan") {
+            String findValue = getValue(payload, ':', 1);
+            findValue.remove(findValue.length() - 1);
+
+            Serial.println("fan");
+
+
+              selectedServo = 3;
+
+            if (findValue == "true") {
+              // write to selectedFanSpeed 
+              fanToggle = true;
+              yield();
+
+              Serial.println("fan on");
+              pulselen = map(selectedFanSpeed, 0, 180, 0, 4096);
+
+
+              Serial.printf("pulse .. %d \n", pulselen);
+
+              pwm.setPWM(selectedServo, 0, pulselen);
+            }
+            else if (findValue == "false") {
+              // set speed to zero
+              fanToggle = false;
+              yield();
+
+              Serial.println("fan off");
+              pwm.setPWM(selectedServo, 0, 0);
+            }
 
           }
-          */
+          else if (lookup_val == "fanSpeed") {
+
+            Serial.println("fanSpeed");
+
+            selectedServo = 3;
+
+            String findValue = getValue(payload, ':', 1);
+            findValue.remove(findValue.length() - 1);  
+            selectedFanSpeed = findValue.toInt();
+
+            if (fanToggle) {
+
+              pulselen = map(selectedFanSpeed, 0, 180, 0, 4096);
+
+              Serial.printf("pulse .. %d \n", pulselen);
+
+              pwm.setPWM(selectedServo, 0, pulselen);
+
+            }
+
+          
+          }
+         
 
 
           // sketch confirms the value by sending it back on /[path]/[confirm]/[device_name]/[endpoint_key]
+          yield();
 
           confirmPath = "";
           confirmPath = thisDevicePath;
@@ -245,6 +458,8 @@ Ticker ticker;
 
           //sendConfirm = true;
           client.publish(MQTT::Publish(confirmPath, confirmPayload).set_qos(2));
+
+        }
 
       }
     }
@@ -303,9 +518,12 @@ void setup() {
   local_ip_str = WiFi.localIP().toString();
   Serial.println(local_ip_str);
 
-  firstServo.attach(PIN_A);
+  DS18B20.begin();
 
+  //Wire.begin(4, 5);
 
+  pwm.begin();
+  pwm.setPWMFreq(50);  
 }
 
 
@@ -344,38 +562,117 @@ void loop() {
           Serial.println("request states sent");
       }
     }
-
-    if (client.connected()) {
-      client.loop();
+    else {
+      client.loop();  
     }
 
-  // -- servo move
-  if (selectedPos != actualPos) {
-    if (millis() - lastMove > moveLimit) {
-      lastMove = millis();
-      firstServo.write(selectedPos);
-        /*
-        if (selectedPos < actualPos) {
-          pos = actualPos - 1;
-          firstServo.write(pos);
-          actualPos = pos;
-        }
-        else if (selectedPos > actualPos) {
-          pos = actualPos + 1;
-          firstServo.write(pos);
-          actualPos = pos;
-        }
-        */
-    }
+
+
+  
+
+
+  if (servoFlag) {
+    // call function: selectedServo, selectedPos
+    servoFlag = false;
+    Serial.println("servoRun");
+    servoRun();
   }
-
 
 
   // Do things every tickLimit seconds
   if ( millis() - tick > tickLimit) {
     tick = millis();
+
+    counter += 1;
+
+    // sketch confirms the value by sending it back on /[path]/[confirm]/[device_name]/[endpoint_key]
+    yield();
+    client.loop();
+
+    getTemperature();
+
+    confirmPath = "";
+    confirmPath = thisDevicePath;
+    confirmPath += "/confirm/";
+    confirmPath += thisDeviceName;
+    confirmPath += "/";
+    confirmPath += "temperature";
+
+    confirmPayload = "{\"update\": {\"labels\":[";
+    confirmPayload += String(counter);
+    confirmPayload += "],\"series\":[[";
+    confirmPayload += String(tempF);
+    confirmPayload += "]]}}";
+
+
+    t = millis(); 
+    client.publish(MQTT::Publish(confirmPath, confirmPayload).set_qos(2));
+    Serial.printf("Publish took %dms\n", millis() - t);
+
+    t = millis();
+    client.loop();
+    Serial.printf("Loop took %dms\n", millis() - t);
+
+
+
+    /*
+    // i2c scan
+    byte error, address;
+    int nDevices;
+   
+    Serial.println("Scanning...");
+   
+    nDevices = 0;
+    for(address = 1; address < 127; address++ ) 
+    {
+   
+      Wire.beginTransmission(address);
+      error = Wire.endTransmission();
+   
+      if (error == 0)
+      {
+        Serial.print("I2C device found at address 0x");
+        if (address<16) 
+          Serial.print("0");
+        Serial.print(address,HEX);
+        Serial.println("  !");
+   
+        nDevices++;
+      }
+      else if (error==4) 
+      {
+        Serial.print("Unknow error at address 0x");
+        if (address<16) 
+          Serial.print("0");
+        Serial.println(address,HEX);
+      }    
+    }
+    if (nDevices == 0)
+      Serial.println("No I2C devices found\n");
+    else
+      Serial.println("done\n");
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 
 
   yield();
+
 }
+
+
+// add 2 more servos and save as separate sketch
--- sketches/three_servos/src/namepins.h
+++ sketches/three_servos/src/namepins.h
@@ -1,4 +1,7 @@
 #define PIN_A 2
+#define PIN_B 5
+#define PIN_C 12
+#define PIN_D 13
 String thisDeviceName = "backToBasic";
 String thisDevicePath = "/esp";
 String subscribe_path = "/esp/#";
